1c46cfea3cb11272d571fab4046ee991c32ebe88
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-06-22 22:06:25 +0200

    [TASK] Use PostreSQL APT repository

--- image/enable_repos.sh
+++ image/enable_repos.sh
@@ -9,6 +9,9 @@ echo deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main > /etc/ap
 ## NGINX Stable Releases
 echo deb http://ppa.launchpad.net/nginx/stable/ubuntu trusty main > /etc/apt/sources.list.d/nginx-stable.list
 
+## PostgreSQL Global Development Group (PGDG)repository
+echo deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main > /etc/apt/sources.list.d/pgdg.list
+
 ## Phusion Passenger
 if [[ "$PASSENGER_ENTERPRISE" ]]; then
 	echo deb https://download:$PASSENGER_ENTERPRISE_DOWNLOAD_TOKEN@www.phusionpassenger.com/enterprise_apt trusty main > /etc/apt/sources.list.d/passenger.list
@@ -33,6 +36,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	561F9B9CAC40B2F7 \
 	5862E31D \
 	C300EE8C \
+	ACCC4CF8 \
 	DA1A4A13543B466853BAF164EB9B1D8886F44E2A
 
 ## NodeSource's Node.js repository
