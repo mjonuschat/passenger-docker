c7b7aa336903bb715aed0b8c6f6bf86ef935fef9
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-07-11 08:32:13 +0200

    [FEATURE] Improve progress reporting

--- image/python.sh
+++ image/python.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 source /pd_build/buildconfig
-set -x
+
+header "Installing Python..."
 
 ## Install Python.
-minimal_apt_get_install python python2.7 python3
+run apt-get install -y python python2.7 python3
